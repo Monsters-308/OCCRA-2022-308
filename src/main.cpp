3a53aa4db720c53c5d7827e947b2da47d929425d
Austin Spencer <mamichalske@yahoo.com>
2022-09-22 19:57:50 -0400

    Init Commit
    First commit for the project

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       FRC                                                       */
+/*    Created:      Thu Sep 22 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+
+    Brain.Screen.printAt( 10, 50, "Hello V5" );
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
