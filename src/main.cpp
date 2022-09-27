2f33d17c5fe80c52da1fd9f1547e152aaedeb4dc
Austin Spencer <mamichalske@yahoo.com>
2022-09-27 19:47:55 -0400

    Vex Code Pro Update
    Updated code to use the Vex Code Pro IDE

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       FRC                                                       */
-/*    Created:      Thu Sep 22 2022                                           */
+/*    Author:       C:\Users\FRC                                              */
+/*    Created:      Tue Sep 27 2022                                           */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
 #include "vex.h"
 
 using namespace vex;
@@ -13,16 +17,15 @@ using namespace vex;
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
 
-// define your global instances of motors and other devices here
-
-
 int main() {
-
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  // vexcodeInit();
+  Brain.Screen.printAt( 10, 50, "Hello V5" );
    
     while(1) {
         
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
+  
 }
